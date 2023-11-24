c5137710d3a52b19c9ce56cb13703f1806e229c5
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-11-24 13:52:07 +0200

    update settings for render and install gunicorn

--- crewing_company_site_config/settings.py
+++ crewing_company_site_config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default="django-insecure-mxfam@dl6itr9iauq$m(9$430!se$^(c92ud6+99h^*h)*qc!9")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
