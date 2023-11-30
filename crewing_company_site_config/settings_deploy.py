fb1390183499d161db877a3aca9004e4286d7ce6
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-11-30 12:22:44 +0200

    divided settings for deploy and base

--- crewing_company_site_config/settings_deploy.py
+++ crewing_company_site_config/settings_deploy.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", default="django-insecure-mxfam@dl6itr9iauq$m(9$430!se$^(c92ud6+99h^*h)*qc!9")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "creqing-company-site.onrender.com"]
 
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crewing.templatetags.custom_filter",
     "crispy_bootstrap4",
+
 ]
 
 MIDDLEWARE = [
